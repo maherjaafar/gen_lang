b49918457a8edb843a1642bcd00ab88c94692db5
Maher <34738373+maherjaafar@users.noreply.github.com>
2022-01-04 12:09:25 +0100

    clean up code and add generated files

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -10,66 +10,55 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-
-  int index = 0;
-  List<Locale> localeList = [Locale('en'), Locale('ja'), Locale('zh', 'TW')];
-
-  @override
-  void initState() {
-    super.initState();
-  }
+  var index = 0;
+  final localeList = <Locale>[Locale('en'), Locale('ja'), Locale('zh', 'TW')];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      localizationsDelegates: [
-        S.delegate,
-        GlobalMaterialLocalizations.delegate,
-        GlobalWidgetsLocalizations.delegate
-      ],
-      supportedLocales: S.delegate.supportedLocales,
-      locale: localeList[index],
-      home: Builder(
-        builder: (BuildContext context) {
-          return Scaffold(
-            appBar: AppBar(
-              title: Text(S.of(context).locale),
-            ),
-            body: Center(
-              child: Column(
-                children: <Widget>[
-                  Text(S.of(context).locale),
-                  Text(S.of(context).simpleMessage),
-                  Text(S.of(context).messageWithParams('developer')),
-                  Text(S.of(context).pluralMessage(1, 'KingWu')),
-                  Text(S.of(context).pluralMessage(10, 'KingWu')),
-                  Text(S.of(context).genderMessage('male', 'KingWu')),
-                  Text(S.of(context).genderMessage('female', 'KingWu')),
-                  Text(S.of(context).genderMessage('other', 'KingWu')),
-                  Text(S.of(context).specialCharactersMessage)
-                ],
-              )
-            ),
-            floatingActionButton: FloatingActionButton(
-              onPressed: () {
-                switchLang();
-              },
-              child: Icon(Icons.add),
-            )
-          );
-        },
-      )
-
-    );
+        localizationsDelegates: [
+          S.delegate,
+          GlobalMaterialLocalizations.delegate,
+          GlobalWidgetsLocalizations.delegate
+        ],
+        supportedLocales: S.delegate.supportedLocales,
+        locale: localeList[index],
+        home: Builder(
+          builder: (BuildContext context) {
+            return Scaffold(
+                appBar: AppBar(
+                  title: Text(S.of(context).locale),
+                ),
+                body: Center(
+                    child: Column(
+                  children: <Widget>[
+                    Text(S.of(context).locale),
+                    Text(S.of(context).simpleMessage),
+                    Text(S.of(context).messageWithParams('developer')),
+                    Text(S.of(context).pluralMessage(1, 'KingWu')),
+                    Text(S.of(context).pluralMessage(10, 'KingWu')),
+                    Text(S.of(context).genderMessage('male', 'KingWu')),
+                    Text(S.of(context).genderMessage('female', 'KingWu')),
+                    Text(S.of(context).genderMessage('other', 'KingWu')),
+                    Text(S.of(context).specialCharactersMessage)
+                  ],
+                )),
+                floatingActionButton: FloatingActionButton(
+                  onPressed: () {
+                    switchLang();
+                  },
+                  child: Icon(Icons.add),
+                ));
+          },
+        ));
   }
 
-  void switchLang(){
+  void switchLang() {
     setState(() {
       index++;
-      if(index >= localeList.length){
+      if (index >= localeList.length) {
         index = 0;
       }
     });
-
   }
 }
